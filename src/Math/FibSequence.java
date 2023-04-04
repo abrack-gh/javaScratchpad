20ea675b7b1cc29eca31856ca72dd43060817d48
Alex Brackenridge <a.brack@live.co.uk>
2023-04-04 21:02:36 +0100

    Scratch Pad

--- src/Math/FibSequence.java
+++ src/Math/FibSequence.java
@@ -1,2 +1,19 @@
-package Math;public class FibSequence {
+package Math;
+
+public class FibSequence {
+
+    public static void main(String args[]){
+
+        int w = 0, x = 1, y, z, count=20;
+        System.out.println("Results of Fibonacci Sequence: ");
+
+        System.out.print(w + " " + x);
+
+        for(z = 2; z < count; z++){
+            y = w + x;
+            System.out.print(" " + y);
+            w = x;
+            x = y;
+        }
+    }
 }
