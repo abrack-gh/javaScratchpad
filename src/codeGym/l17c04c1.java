3a7cbe1b4d7d4a785aaec61cf93ed2c8a600dd2f
Alex Brackenridge <a.brack@live.co.uk>
2023-05-05 09:11:03 +0100

    CodeGym challenges

---
+++ src/codeGym/l17c04c1.java
@@ -0,0 +1,31 @@
+package codeGym;
+
+import java.time.LocalDate;
+
+public class l17c04c1 {
+
+    public static void main(String[] args) {
+        System.out.println(nowExample());
+        System.out.println(ofExample());
+        System.out.println(ofYearDayExample());
+        System.out.println(ofEpochDayExample());
+    }
+
+    static LocalDate nowExample() {
+
+        return LocalDate.now();
+    }
+
+    static LocalDate ofExample() {
+        return LocalDate.of(2020, 9, 12);
+    }
+
+    static LocalDate ofYearDayExample() {
+        return LocalDate.ofYearDay(2020, 256);
+    }
+
+    static LocalDate ofEpochDayExample() {
+        LocalDate date = LocalDate.ofEpochDay(18517);
+        return date;
+    }
+}
---
+++ src/codeGym/l17c4c2.java
@@ -0,0 +1,22 @@
+package codeGym;
+import java.time.LocalDate;
+        import java.time.format.TextStyle;
+        import java.util.Locale;
+
+/*
+Just a day of the week
+*/
+
+public class l17c4c2 {
+
+    static LocalDate birthDate = LocalDate.of(2020, 3, 12);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(LocalDate date) {
+
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    }
+}
