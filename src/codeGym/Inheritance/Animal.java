735583adfa4388f9fb1a195bb22fc644a92546fa
Alex Brackenridge <a.brack@live.co.uk>
2023-05-26 17:38:32 +0100

    Changes to inheritance package

--- src/codeGym/Inheritance/Animal.java
+++ src/codeGym/Inheritance/Animal.java
@@ -1,13 +1,18 @@
 package codeGym.Inheritance;
 
 public class Animal {
-    public static void main(String[] args)
-    {
+
+    public static void printAll(){
+
         Cat cat = new Cat();
         Dog dog = new Dog();
 
         cat.hasLegs();
         dog.sayWoof();
 
+    }
+    public static void main(String[] args)
+    {
+        printAll();
     }
 }
